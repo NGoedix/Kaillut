93d96eb54668f80d55e4b14a79cb835bf3c42716
Goedix <goedix@wearehackerone.com>
2021-12-01 19:02:46 +0100

    First commit

---
+++ src/pages/Index/Main.jsx
@@ -0,0 +1,14 @@
+import React, { /*useState*/ } from 'react'
+
+import '../../styles.css'
+
+export function Main() {
+  //const [] = useState();
+  return (
+    <React.Fragment>
+      <div>
+        
+      </div>
+    </React.Fragment>
+  )
+}
