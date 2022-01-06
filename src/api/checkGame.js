f986722d6afc681ef38bacd78b0dff9c5d28f933
Goedix <goedix@wearehackerone.com>
2022-01-06 16:12:58 +0100

    Removed http lib

--- src/api/checkGame.js
+++ src/api/checkGame.js
@@ -1,7 +1,9 @@
 const http = require('./libs/http')
 
 export async function checkGame(game) {
-  let data = await http.getRequest('/api/room/' + game);
+  /*let data = await http.getRequest('/api/room/' + game);
 
-  console.log(data);
+  console.log(data);*/
+
+  return true;
 }
