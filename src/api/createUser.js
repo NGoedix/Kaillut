f51c4aef9524c200a38d5d165e885419a8acea82
Goedix <goedix@wearehackerone.com>
2022-01-15 15:46:53 +0100

    fix(Root): Hotfix error handler with the API.

--- src/api/createUser.js
+++ src/api/createUser.js
@@ -1,41 +1,34 @@
 const http = require('./libs/http')
 
 export async function createUser({email, username, password, role}) {
-    
-    const options = {
-        headers: { 'Content-type': 'application/json' }
-    };
 
     const data = JSON.stringify({ email: email, username: username, password: password, role: role })
 
     const res = await http.postRequest('/api/account/register', data);
     
+    console.log(res);
+
     if (res.success === false) {
-        switch (res.error) {
-            case 'internal_error':
-                return 'Error interno.'
-            case 'role_type_not_match':
-                return 'Rol no válido.'
-            case 'username_unavailable':
-                return 'Nombre de usuario no disponible.'
-            case 'password_length_not_match':
-                return 'La contraseña no puede contener espacios.'
-            case 'password_must_longest':
-                return 'La contraseña debe contener mínimo 8 carácteres.'
-            case 'password_regex_not_match':
-                return 'La contraseña debe contener 1 mayúscula, 1 minúscula, 1 número y 1 carácter especial.'
+        if (res.error == 'internal_error') return 'Error interno.';
+        if (res.error == 'email_or_username_unavailable') return 'Usuario o email ya registrados.';
+        if (res.error.email == 'email_regex_not_match') return 'El email no es válido.';
+        if (res.error.role == 'role_type_not_match') return 'El rol no es válido.'
+        switch (res.error.user) {
             case 'username_length_not_match':
                 return 'El usuario no puede contener espacios.'
             case 'username_must_longest':
                 return 'El nombre de usuario debe contener mínimo 4 carácteres.'
             case 'username_regex_not_match':
                 return 'El nombre de usuario solo puede contener mayúsculas, minúculas, puntos y guiones.'
-            case 'email_regex_not_match':
-                return 'El email no es válido.'
-            case 'email_unavailable':
-                return 'El email ya está registrado.'
+        }
+        switch (res.error.password) {
+            case 'password_length_not_match':
+                return 'La contraseña no puede contener espacios.'
+            case 'password_must_longest':
+                return 'La contraseña debe contener mínimo 8 carácteres.'
+            case 'password_regex_not_match':
+                return 'La contraseña debe contener 1 mayúscula, 1 minúscula, 1 número y 1 carácter especial.'
         }
       }
       return true;
-
 }
